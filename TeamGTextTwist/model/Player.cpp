d60f43fc46b4bc755bf27787df0217d7d8e0c90c
dwalke22 <dwalke22@my.westga.edu>
2021-05-02 12:28:40 -0400

    Added documentation for the player class

--- TeamGTextTwist/model/Player.cpp
+++ TeamGTextTwist/model/Player.cpp
@@ -2,6 +2,17 @@
 
 namespace model
 {
+//Constructor for the player
+//
+//@param name
+//      the name of the player
+//@param score
+//      the score of the player
+//@param time
+//      the time for the player
+//
+//@precondition none
+//@postcondition none
 Player::Player(const string& name, int score, int time)
 {
     this->name = name;
@@ -14,6 +25,12 @@ Player::~Player()
     //dtor
 }
 
+//Returns a string representation of the player
+//
+//@precondition none
+//@postcodition none
+//
+//@returns string representation of the player
 const string Player::toString()
 {
     return this->name + " " + to_string(this->score) + " points " + to_string(this->time) + " seconds";
